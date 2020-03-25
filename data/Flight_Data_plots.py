d895ad43c6e0e7e368111085ba7e75319570f030
Evert-DV <60961664+Evert-DV@users.noreply.github.com>
2020-03-25 14:10:23 +0100

    Update Flight_Data_plots.py

--- data/Flight_Data_plots.py
+++ data/Flight_Data_plots.py
@@ -1,7 +1,6 @@
 from numpy import *
 from Flight_Data import DATA
 from matplotlib import pyplot as plt
-from Num_Mod import Ba
 
 #=======================================================================================
 
@@ -19,7 +18,7 @@ full = [9,4564,'full flight','']
 
 custom = [3400,3600,'custom timeframe',''] # USER INPUT
 
-motion = custom # choose motion # USER INPUT
+motion = dutch_roll # choose motion # USER INPUT
 
 
 i_0 = where(DATA[2:,48].astype(float) == motion[0])
@@ -59,51 +58,31 @@ else: # customize, user input
     y_ax3 = 26
     y_ax4 = 28
 
+#===================================================================================
+# TEST
+
+t = DATA[indexes,48].astype(float)
+ 
+bèta = DATA[indexes,49].astype(float)
+phi = DATA[indexes,21].astype(float)
+p = DATA[indexes,26].astype(float)
+r = DATA[indexes,28].astype(float)
+
+value = p
+
+peaks = where(abs(hstack((0,value))[:-1]-value) < 0.05)
+peaks = peaks[0][where(value[peaks] > average(value[peaks]))]
+xp = t[peaks]
+fp = value[peaks]
+a,b,c = polyfit(xp,fp,2)
+y = a*t**2 + b*t + c
+
+a0 = y[0]
+i_half = where(abs(y-a0/2) == min(abs(y-a0/2)))
+
+T_half = t[i_half]-t[0]
 
 #=====================================================================================
-
-# EIGENVALUES
-
-###create state vector  (always bèta, phi, etc? or make state whichever meas. are chosen?)
-##bèta = DATA[indexes,49].astype(float)
-##phi = DATA[indexes,21].astype(float)
-##p = DATA[indexes,26].astype(float)
-##r = DATA[indexes,28].astype(float)
-##x = array([bèta,phi,p,r])
-##
-##a0_phi = max(abs(max(phi)),abs(min(phi)))
-
-
-
-##if motion[3] == 'A': #for Asymmetric motions
-##
-##    # create state vector  (always bèta, phi, etc? or make state whichever meas. are chosen?)
-##    bèta = DATA[indexes,49].astype(float)
-##    phi = DATA[indexes,21].astype(float)
-##    p = DATA[indexes,26].astype(float)
-##    r = DATA[indexes,28].astype(float)
-##    x = mat([bèta,phi,p,r])
-##
-##    # input vector (for assymetrical motion)
-##    d_e = DATA[indexes,16].astype(float)
-##    d_r = DATA[indexes,18].astype(float)
-##    u = mat([d_e,d_r])
-##
-##    x_dot = array([[0],[0],[0],[0]])
-##    for i in range(1,len(indexes-1)):
-##        xdot = (x[:,i]-x[:,i-1])/0.1
-##        x_dot = hstack((x_dot,xdot))
-##    x_dot = mat(x_dot)
-##    # or is xdot depending on the meas. chosen? eg: choose x = [roll angle, pitch angle],
-##    # so xdot = [roll rate, pitch rate]? but then see comment on state vector again
-##    
-##    A = divide((x_dot-mat(Ba)*u),x) # since: xdot = Ax + Bu
-##    # allowed to use 'Ba' from num. mod.? Since the B matr only contains given A/C parameters, right?
-##    # trying to do this: (xdot - B*u)/x = A, so to divide I do dotproduct with 1/(x.transposed)?
-##    # only this way I can get A to be square
-##    # waarom schrijf ik dit in het engels
-##
-##    eigen_val = linalg.eigvals(A)
 
 dom = indexes
 
@@ -118,28 +97,28 @@ col2 ='b' # color for y axis 2
 fig, (ax1,ax3) = plt.subplots(2,1,sharex=True)
 
 #subplot 1
-ax1.plot(DATA[dom,x_ax].astype(float),DATA[dom,y_ax].astype(float),color=col1)
+ax1.plot(t,y,color=col1)
 ax1.set_xlabel(DATA[0,x_ax]+' ['+DATA[1,x_ax]+']')
 ax1.set_ylabel(DATA[0,y_ax]+' ['+DATA[1,y_ax]+']',color=col1)
 ax1.tick_params(axis='y', labelcolor=col1)
 
 ax2 = ax1.twinx()
 
-ax2.plot(DATA[dom,x_ax].astype(float),DATA[dom,y_ax2].astype(float),color=col2)
+ax2.plot(t,DATA[dom,y_ax2].astype(float),color=col2)
 ax2.set_ylabel(DATA[0,y_ax2]+' ['+DATA[1,y_ax2]+']',color=col2)
 ax2.tick_params(axis='y', labelcolor=col2)
 
 plt.title(DATA[0,y_ax]+' & '+DATA[0,y_ax2]+' over '+DATA[0,x_ax]+'\nfor '+motion[2])
 
 #subplot 2
-ax3.plot(DATA[dom,x_ax].astype(float),DATA[dom,y_ax3].astype(float),color=col1)
+ax3.plot(t,DATA[dom,y_ax3].astype(float),color=col1)
 ax3.set_xlabel(DATA[0,x_ax]+' ['+DATA[1,x_ax]+']')
 ax3.set_ylabel(DATA[0,y_ax3]+' ['+DATA[1,y_ax3]+']',color=col1)
 ax3.tick_params(axis='y', labelcolor=col1)
 
 ax4 = ax3.twinx()
 
-ax4.plot(DATA[dom,x_ax].astype(float),DATA[dom,y_ax4].astype(float),color=col2)
+ax4.plot(t,DATA[dom,y_ax4].astype(float),color=col2)
 ax4.set_ylabel(DATA[0,y_ax4]+' ['+DATA[1,y_ax4]+']',color=col2)
 ax4.tick_params(axis='y', labelcolor=col2)
 
@@ -201,3 +180,47 @@ plt.show()
     47='Active Screen'
     48='Time'
     49 = sideslip angle  (calculated arctan('33'/'32') ) """
+
+
+# EIGENVALUES
+
+###create state vector  (always bèta, phi, etc? or make state whichever meas. are chosen?)
+##bèta = DATA[indexes,49].astype(float)
+##phi = DATA[indexes,21].astype(float)
+##p = DATA[indexes,26].astype(float)
+##r = DATA[indexes,28].astype(float)
+##x = array([bèta,phi,p,r])
+##
+##a0_phi = max(abs(max(phi)),abs(min(phi)))
+
+
+
+##if motion[3] == 'A': #for Asymmetric motions
+##
+##    # create state vector  (always bèta, phi, etc? or make state whichever meas. are chosen?)
+##    bèta = DATA[indexes,49].astype(float)
+##    phi = DATA[indexes,21].astype(float)
+##    p = DATA[indexes,26].astype(float)
+##    r = DATA[indexes,28].astype(float)
+##    x = mat([bèta,phi,p,r])
+##
+##    # input vector (for assymetrical motion)
+##    d_e = DATA[indexes,16].astype(float)
+##    d_r = DATA[indexes,18].astype(float)
+##    u = mat([d_e,d_r])
+##
+##    x_dot = array([[0],[0],[0],[0]])
+##    for i in range(1,len(indexes-1)):
+##        xdot = (x[:,i]-x[:,i-1])/0.1
+##        x_dot = hstack((x_dot,xdot))
+##    x_dot = mat(x_dot)
+##    # or is xdot depending on the meas. chosen? eg: choose x = [roll angle, pitch angle],
+##    # so xdot = [roll rate, pitch rate]? but then see comment on state vector again
+##    
+##    A = divide((x_dot-mat(Ba)*u),x) # since: xdot = Ax + Bu
+##    # allowed to use 'Ba' from num. mod.? Since the B matr only contains given A/C parameters, right?
+##    # trying to do this: (xdot - B*u)/x = A, so to divide I do dotproduct with 1/(x.transposed)?
+##    # only this way I can get A to be square
+##    # waarom schrijf ik dit in het engels
+##
+##    eigen_val = linalg.eigvals(A)
